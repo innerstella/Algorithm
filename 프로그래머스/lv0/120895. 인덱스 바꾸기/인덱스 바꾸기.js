4a8b9dc5c05737ca2593617e7d9f8d7901f77f79
Suji Cha <77491430+innerstella@users.noreply.github.com>
2022-12-05 18:12:01 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+    var answer = '';
+    my_string = my_string.split('');
+    let temp = my_string[num1];
+    my_string[num1] = my_string[num2];
+    my_string[num2] = temp;
+    answer = my_string.join('');
+    return answer;
+}
