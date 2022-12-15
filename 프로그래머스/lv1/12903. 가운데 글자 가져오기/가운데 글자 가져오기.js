0bbd857f7af52126d38c6fd14fe4ff7900b762bc
Suji Cha <77491430+innerstella@users.noreply.github.com>
2022-12-16 00:24:51 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    var answer = '';
+    
+    if(s.length%2 === 1){
+        answer = s[Math.floor(s.length/2)]
+    } else {
+        answer = s[Math.floor(s.length/2)-1] + s[Math.floor(s.length/2)]
+    }
+    return answer;
+}
