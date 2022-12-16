c39805651fe518d39bd6ee03e1ffacfa8f7e31e3
Suji Cha <77491430+innerstella@users.noreply.github.com>
2022-12-16 19:51:02 +0900

    [level 1] Title: 소수 찾기, Time: 29.52 ms, Memory: 45.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = isSosu(n);
+    return answer;
+}
+
+function isSosu(n){
+    let arr = Array(n+1).fill(true).fill(false, 0, 2);
+    for (let i=2; i*i<=n; i++){
+        if(arr[i]){
+            for(let j=i*i; j<=n; j+=i){
+                arr[j] = false;
+            }
+        }
+    }
+    return arr.filter(e => e).length;
+}
