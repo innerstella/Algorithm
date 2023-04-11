98fdc78885f3580611b9f81e6b59e58790704047
차수지 <77491430+innerstella@users.noreply.github.com>
2023-04-11 23:43:13 +0900

    [level 2] Title: 타겟 넘버, Time: 1.71 ms, Memory: 35.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,21 @@
+function solution(numbers, target) {
+    let answer = 0;
+    let length = numbers.length;
+    
+    const dfs = (count, sum) => {
+        if(count === length){
+            if (target === sum){
+                answer++;
+            }
+        return;
+        }
+        
+        dfs(count+1, sum + numbers[count]);
+        dfs(count+1, sum - numbers[count]);
+    }
+    
+    dfs(0,0)
+    
+    
+    return answer;
+}
