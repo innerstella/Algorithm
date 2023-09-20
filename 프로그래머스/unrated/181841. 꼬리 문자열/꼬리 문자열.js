e110962abbc115f5147c40dc48414fdbff9942a5
차수지 <77491430+innerstella@users.noreply.github.com>
2023-09-20 17:00:38 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,10 @@
+function solution(str_list, ex) {
+    let answer = ''
+    str_list.forEach((str)=>{
+        if(!str.includes(ex)){
+            answer += str
+        }
+    })
+    
+    return answer
+}
