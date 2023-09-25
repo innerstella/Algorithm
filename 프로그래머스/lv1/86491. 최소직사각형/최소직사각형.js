6f6fa5c1d6503926baaef5402e62971795560362
차수지 <77491430+innerstella@users.noreply.github.com>
2023-09-25 15:03:37 +0900

    [level 1] Title: 최소직사각형, Time: 2.08 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,17 +1,16 @@
 function solution(sizes) {
-    var answer = 0;
-    
-    let X = [];
-    let Y = [];
-    sizes.map((x)=>{
-        X.push(Math.max(...x));
-        Y.push(Math.min(...x));
+    // 1. 지갑 정리하기
+    let arranged = sizes.map((wallet)=>{
+        if(wallet[0] < wallet[1]){
+            return [wallet[1], wallet[0]]
+        } else {
+            return wallet
+        }
     })
-
-    let x_max = Math.max(...X);
-    let y_max = Math.max(...Y);
-
-    answer = x_max * y_max;
     
-    return answer;
+    // 2. 최대 가로 길이, 최대 세로 길이 
+    let x = arranged.map((wallet)=>wallet[0])
+    let y = arranged.map((wallet)=>wallet[1])
+    
+    return Number(Math.max(...x))*Number(Math.max(...y))
 }
