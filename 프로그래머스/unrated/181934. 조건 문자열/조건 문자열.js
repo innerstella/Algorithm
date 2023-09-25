251d86f9497a6383717f708087781e1208a10721
차수지 <77491430+innerstella@users.noreply.github.com>
2023-09-25 14:26:11 +0900

    [unrated] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,30 @@
+function solution(ineq, eq, n, m) {
+    if(ineq === '>' && eq === '='){
+        if(n >= m){
+            return 1
+        } else {
+            return 0
+        }
+    }
+    else if(ineq === '<' && eq === '='){
+        if(n <= m){
+            return 1
+        } else {
+            return 0
+        }
+    }
+    else if(ineq === '>' && eq === '!'){
+        if(n > m){
+            return 1
+        } else {
+            return 0
+        }
+    }
+    else if(ineq === '<' && eq === '!'){
+        if(n < m){
+            return 1
+        } else {
+            return 0
+        }
+    }
+}
